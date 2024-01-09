7f16faa9e63a9d1adfa956ea4ccc1dc27cd6cb8a
Rodolphe <dutchr@live.fr>
2024-01-09 17:10:11 +0100

    :recycle: Refactoring method saveUser + adding bcrypt for hashing password

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,10 @@ import User from "../models/userModel";
 
 const saveUser = async (req, res) => {
   try {
-    const newUser = await User.create(req.body);
+    const newUser = await new User();
+    newUser.email = req.body.email;
+    newUser.password = await newUser.crypto(req.body.password);
+    newUser.save();
     res.json(newUser);
   } catch (error) {
     console.error(error);
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,10 +1,16 @@
 import { mongoose, Schema } from "mongoose";
-
+import bcrypt from "bcryptjs";
 const userSchema = new Schema({
   name: String,
   email: { type: String, unique: true },
   password: { type: String, min: [6, "Must be at least 6 characters"] },
 });
+
+userSchema.methods.crypto = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+};
 
 const User = mongoose.model("User", userSchema);
 
