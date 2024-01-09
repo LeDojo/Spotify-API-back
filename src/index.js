f26b79c3b363d5a217eee4f0f43fd915d1e78341
Rodolphe <dutchr@live.fr>
2024-01-09 11:08:33 +0100

    :card_file_box: create connexion to MongoDB w/ mongoose :card_file_box:

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
-const app = express()
-const port = process.env.PORT
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
+import mongoose from "mongoose";
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/spotify_db");
+  console.log(`[📚 DATABASE ] MongoDB est connecté !!`);
+}
+app.get("/", (req, res) => res.send("WELCOME API SPOTIFY"));
 
-app.get('/', (req, res) => console.log('WELCOME API SPOTIFY'))
-
-
-app.listen(port, () => console.log(`[SERVER] is running on http://localhost:${port}`))
+app.listen(port, () =>
+  console.log(`[SERVER] is running on http://localhost:${port}`)
+);
